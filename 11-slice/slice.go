5ee5a916b3201002be61c40042522919b5f5728a
strick <strick@yak.net>
2022-10-22 12:46:24 -0700

    gofmt

--- 11-slice/slice.go
+++ 11-slice/slice.go
@@ -12,27 +12,27 @@ func main() {
 	flag.Parse()
 	n := *nFlag
 
-    // Make a slice of bool, pre-allocating n slots.
-    // They are automatically initialized to the "zero value" for bool, which is false.
+	// Make a slice of bool, pre-allocating n slots.
+	// They are automatically initialized to the "zero value" for bool, which is false.
 	sieve := make([]bool, n)
-    // For integers i starting at 2, mark all the multiples of i, starting at 2*i, as composite.
+	// For integers i starting at 2, mark all the multiples of i, starting at 2*i, as composite.
 	for i := 2; i < n; i++ {
 		for j := 2 * i; j < n; j += i {
 			sieve[j] = true // j is composite.
 		}
 	}
 
-    // Create an empty slice of string to hold our reports.
-    var reports []string
-    // For integers i starting at 2, report that i is prime, if sieve[i] is false.
+	// Create an empty slice of string to hold our reports.
+	var reports []string
+	// For integers i starting at 2, report that i is prime, if sieve[i] is false.
 	for i := 2; i < n; i++ {
 		if !sieve[i] {
-            // Append a string to the reports.
+			// Append a string to the reports.
 			reports = append(reports, fmt.Sprintf("%d is prime.", i))
 		}
 	}
-    // Iterate over reports, printing each one.
-    for _, report := range reports {
-        fmt.Printf("%s\n", report)
-    }
+	// Iterate over reports, printing each one.
+	for _, report := range reports {
+		fmt.Printf("%s\n", report)
+	}
 }
